239cf5ddb5f0e5c1741d623f163ef258459d4db1
Avishka Piyumal <avishkapiyumal16@gmail.com>
2026-04-22 01:24:41 +0530

    Sensor data read

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,15 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const mqtt = require("mqtt");
 require("dotenv").config();
-
-const Sensor = require("./models/Sensor");
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+app.use(express.json());
 
-// MongoDB connect
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+connectDB();
 
-// MQTT connect
-const client = mqtt.connect("mqtt://broker.hivemq.com");
+app.use("/api/sensors", require("./routes/sensorRoutes"));
 
-client.on("connect", () => {
-  console.log("MQTT Connected");
-  client.subscribe("greenhouse/data");
-});
-
-// receive data
-client.on("message", async (topic, message) => {
-  try {
-    const data = JSON.parse(message.toString());
-
-    const newData = new Sensor({
-      temperature: data.temperature,
-      humidity: data.humidity,
-      lux: data.lux,
-      airppm: data.airppm,
-      soil: data.soil,
-      fan: data.fan,
-      buzzer: data.buzzer
-    });
-
-    await newData.save();
-    console.log("Data saved to MongoDB");
-  } catch (err) {
-    console.log("Error:", err);
-  }
-});
-
-// API route (for frontend)
-app.get("/api/data", async (req, res) => {
-  const data = await Sensor.find().sort({ createdAt: -1 }).limit(50);
-  res.json(data);
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
